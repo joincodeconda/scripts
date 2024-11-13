c3b9b44ef223cfe3a6002e9ce3fb00f19435bb8d
CodeConda <123995819+joincodeconda@users.noreply.github.com>
2024-11-13 10:43:40 -0500

    Add files via upload

---
+++ linguana-translate-custom-131124.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function () {
+  if (window.location.pathname.includes("/tr")) {
+    document.querySelectorAll("a").forEach((link) => {
+      if (link.href.includes("app.medicasimple.com")) {
+        link.href = link.href.replace(
+          "app.medicasimple.com",
+          "klinik.medicasimple.com"
+        );
+      }
+    });
+  }
+});
