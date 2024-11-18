4d85d107175eaadef740e8a1e945f319fcde4ad6
CodeConda <123995819+joincodeconda@users.noreply.github.com>
2024-11-18 11:45:36 -0500

    Update linguana-translate-custom-051124.js

--- linguana-translate-custom-051124.js
+++ linguana-translate-custom-051124.js
@@ -59,6 +59,36 @@ window.addEventListener("load", function () {
         "A wide range of activities to meet all your needs. From the live creation of a piece by a street artist for a product launch to a team-building activity aimed at creating a collaborative mural, the advice from our event agency helps you make the right choice!",
       "Si vous avez envie d’organiser un événement ou une activité artistique pour un team building moderne et innovant, n’hésitez pas à nous contacter via le formulaire de contact mis à disposition sur notre site. Nous vous répondrons dans les plus brefs délais.":
         "If you wish to organize an event or an artistic activity for a modern and innovative team building, do not hesitate to contact us via the contact form available on our site. We will respond as soon as possible.",
+      "Avec Street Designers, plongez dès maintenant dans une activité arty, graffiti ou street art ou découvrez les différentes techniques de cette discipline !":
+        "With Street Designers, immerse yourself now in an arty, graffiti or street art activity or discover the different techniques of this discipline!",
+      "Grâce à une ": "Thanks to a ",
+      "activité artistique de graffiti ": "graffiti artistic activity ",
+      "tout le monde peut s’amuser tout en créant une œuvre de qualité.":
+        "everyone can have fun while creating a quality piece.",
+      "Envie de décorer une vitrine ou vos locaux ? Faites appel à notre équipe pour un ":
+        "Want to decorate a storefront or your premises? Contact our team for a ",
+      "design mural sur-mesure.": "custom mural design.",
+      "POURQUOI REALISER UNE ANIMATION D'EQUIPE OU CREATION ARTISTIQUE ?":
+        "WHY ORGANIZE A TEAM ACTIVITY OR ARTISTIC CREATION?",
+      "STREET DESIGNERS : DES ACTIVITES ARTISTIQUES ORIGINALES ADAPTEES A TOUS":
+        "STREET DESIGNERS: ORIGINAL ARTISTIC ACTIVITIES ADAPTED FOR ALL",
+      "En fonction des attentes de nos clients, nous sommes en mesure d’adapter notre offre. En effet, chaque animation artistique est réalisée par nos soins, ce qui nous permet de connaître les     besoins spécifiques de chaque activité. Une animation sur mesure de A à Z qui prend en compte vos attentes ! ":
+        "Depending on the expectations of our clients, we are able to adapt our offer. Indeed, each artistic activity is carried out by us, allowing us to understand the specific needs of each activity. A custom animation from A to Z that takes your expectations into account!",
+      "Grâce à une animation de ": "Thanks to an animation of ",
+      "graffiti à Paris (notamment), ": "graffiti in Paris (notably), ",
+      "plongez dans le monde du street art. Devenez un artiste le temps d’un instant, révélez vos talents cachés et créez des toiles avec un coach à vos côtés. ":
+        "immerse yourself in the world of street art. Become an artist for a moment, reveal your hidden talents, and create canvases with a coach by your side.",
+      "Une large palette d’animations pour répondre à tous vos besoins. De la création d’une œuvre en direct par un street artist pour un lancement de produit en passant par une animation de team building ayant pour but de créer une fresque collaborative, les conseils de l’agence événementielle vous aide à faire le bon choix ! ":
+        "A wide range of activities to meet all your needs. From the live creation of a piece by a street artist for a product launch to a team-building activity aimed at creating a collaborative mural, the advice from our event agency helps you make the right choice!",
+      "Si vous avez envie d’organiser un événement ou une activité artistique pour un ":
+        "If you wish to organize an event or an artistic activity for a ",
+      "team building moderne et innovant, ":
+        "modern and innovative team building, ",
+      "n’hésitez pas à nous contacter via le ":
+        "do not hesitate to contact us via the ",
+      "formulaire de contact ": "contact form ",
+      "mis à disposition sur notre site. Nous vous répondrons dans les plus brefs délais.":
+        "available on our site. We will respond as soon as possible.",
     };
 
     function replaceTextInNode(node) {
